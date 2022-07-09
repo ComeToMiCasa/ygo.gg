a7f35d58b9deabd54a456f5423210ab318919a30
ComeToMiCasa <rk.ca.tsiak@kaist.ac.kr>
2022-07-09 15:55:01 +0900

    ADD dotenv functions to webpack

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import reactDom from "react-dom";
-import App from "./src/App"
+import App from "./src/App";
+import { BrowserRouter } from "react-router-dom";
 
-reactDom.render(<App />, document.getElementById("root"));
+reactDom.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Register from "../pages/register";
+import Home from "../pages/home";
+import DeckRegister from "../pages/deckRegister";
+import MatchRegister from "../pages/matchRegister";
+import { Route, Routes } from "react-router-dom";
+import Header from "../components/header";
+import Admin from "../pages/admin";
 
 const App = () => {
-  return (
-    <div>
-      <Register />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/match" element={<MatchRegister />} />
+                <Route path="/deck" element={<DeckRegister />} />
+                <Route path="/admin" element={<Admin />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
