aa356ccce8eb8f845e5c4b22398cc179050fb66f
CoutRules <floyd@sasa.hs.kr>
2023-03-30 00:46:19 +0900

    .

---
+++ pages/deckRankingPage.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "../styles/deck.css"
+
+const DeckRankingPage = () => {
+	return (
+		<div>
+            
+		</div>
+	)
+}
+
+const DeckRankingComponent = ({ name, decks, width, height, top, left }) => {
+	return (
+		<div
+			className="DeckRankingComponent"
+			style={{
+
+			}}
+		>
+            
+		</div>
+	)
+}
+
+export default DeckRankingPage
